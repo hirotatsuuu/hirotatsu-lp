4730fdb895b561ca11795dd64c4dca60056fb555
hirotatsu <hirotatsuuu@gmail.com>
2019-01-26 22:30:02 +1300

    audioの追加

--- public/main.js
+++ public/main.js
@@ -4,3 +4,24 @@ var scroll = new SmoothScroll('a[href*="#"]', {
     updateURL: false,
 	speedAsDuration: true
 });
+
+// audio
+document.getElementById('audio').play();
+document.getElementById('audio').pause();
+
+$(function () {
+    var audioBtn = $('.audio_button'),
+        audioWrap = $('.audio_wrap'),
+        audio = document.getElementById('audio');
+
+    audioBtn.on('click', function () {
+        if( audioWrap.hasClass('play') ) {
+            audio.pause();
+            audioWrap.removeClass('play');
+        } else {
+            audio.play();
+            audioWrap.addClass('play');
+        }
+    });
+});
+
